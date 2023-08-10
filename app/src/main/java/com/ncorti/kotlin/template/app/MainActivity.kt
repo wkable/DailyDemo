84a02a5ae15ad93539138584e2ea1cfff10e21e1
wkable <kun0119870924@126.com>
2023-08-10 17:09:01 +0800

    clean: fix imports.

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -2,14 +2,12 @@
 
 package com.ncorti.kotlin.template.app
 
-import android.content.pm.PackageParser
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.ncorti.kotlin.template.app.databinding.ActivityMainBinding
-import com.ncorti.kotlin.template.app.plugin.PackageParserCompat
 import com.ncorti.kotlin.template.app.utils.SdPermissionHelper
 import com.ncorti.kotlin.template.app.utils.StorageResult
 import com.ncorti.kotlin.template.app.utils.getPluginDrawableResId
